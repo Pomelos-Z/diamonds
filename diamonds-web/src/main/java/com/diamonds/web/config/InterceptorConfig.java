d7ddd95e6e3b8113b3fb08456a8d7f76e2b32548
Xiaoshen <shaoyashen@fshows.com>
2020-09-08 00:12:03 +0800

    README.md update

--- diamonds-web/src/main/java/com/diamonds/web/config/InterceptorConfig.java
+++ diamonds-web/src/main/java/com/diamonds/web/config/InterceptorConfig.java
@@ -1,5 +1,6 @@
 package com.diamonds.web.config;
 
+import com.diamonds.web.access.AccessInterceptor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -9,9 +10,13 @@ public class InterceptorConfig implements WebMvcConfigurer {
     @Autowired
     private WebInterceptor interceptor;
 
+    @Autowired
+    private AccessInterceptor accessInterceptor;
+
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
         // 加入的顺序就是拦截器执行的顺序
         registry.addInterceptor(interceptor).addPathPatterns("/**");
+        registry.addInterceptor(accessInterceptor).addPathPatterns("/**");
     }
 }
